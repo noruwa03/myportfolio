b2041fd8181f6d8744876bef2ea5e26762c2d4b9
noruwa03 <noruwaobaseki@gmail.com>
2023-05-21 22:36:01 +0100

    added github link to projects

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import MyImage from "./assets/images/Obaseki_Noruwa.jpg";
 import Haraka from "./assets/images/haraka.png";
 import Ecolabz from "./assets/images/ecolabz.png";
 
-
 function getImageUrl(name: string) {
   return new URL(`./assets/images/${name}`, import.meta.url).href;
 }
@@ -739,7 +738,7 @@ function App() {
               <p className="lg:text-base text-sm  font-medium text-slate-800 dark:text-gray-400 mt-3">
                 Ecommerce web app created with tailwindcss and firebase.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 flex space-x-8">
                 <a
                   href="https://ecommerce-firebase-sigma.vercel.app/"
                   target="_blank"
@@ -765,6 +764,29 @@ function App() {
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                   </svg>
                 </a>
+                <a
+                  href="https://github.com/noruwa03/Ecommerce-Firebase"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github stroke-slate-600 dark:stroke-white"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a>
               </div>
             </div>
 
@@ -779,7 +801,7 @@ function App() {
                 Personal blogging site created with nuxt js, tailwindcss and
                 firebase.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 flex space-x-8">
                 <a
                   href="https://draft-blog.vercel.app/"
                   target="_blank"
@@ -805,6 +827,29 @@ function App() {
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                   </svg>
                 </a>
+                <a
+                  href="https://github.com/noruwa03/DraftBlog"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github stroke-slate-600 dark:stroke-white"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a>
               </div>
             </div>
 
@@ -821,7 +866,7 @@ function App() {
               <p className="lg:text-base text-sm  font-medium text-slate-800 dark:text-gray-400 mt-3">
                 Built with HTML, CSS, JavaScript.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 flex space-x-8">
                 <a
                   href="https://dictionary-app-vert.vercel.app/"
                   target="_blank"
@@ -847,6 +892,29 @@ function App() {
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                   </svg>
                 </a>
+                <a
+                  href="https://github.com/noruwa03/DictionaryApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github stroke-slate-600 dark:stroke-white"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a>
               </div>
             </div>
 
@@ -860,7 +928,7 @@ function App() {
               <p className="lg:text-base text-sm  font-medium text-slate-800 dark:text-gray-400 mt-3">
                 Basic TodoApp build with HTML, SCSS, JavaScript.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 flex space-x-8">
                 <a
                   href="https://todo-app-noruwa03.vercel.app/"
                   target="_blank"
@@ -886,6 +954,29 @@ function App() {
                     <line x1="10" y1="14" x2="21" y2="3"></line>
                   </svg>
                 </a>
+                <a
+                  href="https://github.com/noruwa03/TodoApp"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github stroke-slate-600 dark:stroke-white"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
+                  </svg>
+                </a>
               </div>
             </div>
 
@@ -899,7 +990,7 @@ function App() {
               <p className="lg:text-base text-sm  font-medium text-slate-800 dark:text-gray-400 mt-3">
                 NFT collectibles gallery studio.
               </p>
-              <div className="mt-4">
+              <div className="mt-4 flex space-x-8">
                 <a
                   href="https://nordev-nft.netlify.app/"
                   target="_blank"
@@ -923,6 +1014,29 @@ function App() {
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                     <polyline points="15 3 21 3 21 9"></polyline>
                     <line x1="10" y1="14" x2="21" y2="3"></line>
+                  </svg>
+                </a>
+                <a
+                  href="https://github.com/noruwa03/NFT-Marketplace"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="inline-block"
+                >
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    role="img"
+                    viewBox="0 0 24 24"
+                    height="24"
+                    width="24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="feather feather-github stroke-slate-600 dark:stroke-white"
+                  >
+                    <title>GitHub</title>
+                    <path d="M9 19c-5 1.5-5-2.5-7-3m14 6v-3.87a3.37 3.37 0 0 0-.94-2.61c3.14-.35 6.44-1.54 6.44-7A5.44 5.44 0 0 0 20 4.77 5.07 5.07 0 0 0 19.91 1S18.73.65 16 2.48a13.38 13.38 0 0 0-7 0C6.27.65 5.09 1 5.09 1A5.07 5.07 0 0 0 5 4.77a5.44 5.44 0 0 0-1.5 3.78c0 5.42 3.3 6.61 6.44 7A3.37 3.37 0 0 0 9 18.13V22"></path>
                   </svg>
                 </a>
               </div>
